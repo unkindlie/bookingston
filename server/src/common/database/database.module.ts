e3b976e1c80c87d61796e86c654ab81f6bc37301
UnkindlyBiased <inoutoftheverge@gmail.com>
2024-12-29 14:23:47 +0200

    fix: database module shortening

--- server/src/common/database/database.module.ts
+++ server/src/common/database/database.module.ts
@@ -1,25 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { dataSourceOptions } from './datasource.config';
+
 @Module({
-    imports: [
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get('database.host'),
-                username: configService.get('database.username'),
-                password: configService.get('database.password'),
-                port: configService.get('database.port'),
-                database: configService.get('database.name'),
-                entities: [__dirname + '/../../features/**/*.entity.{ts,js}'],
-                migrations: [__dirname + '/migrations/*.{js,ts}'],
-                synchronize: configService.get('NODE_ENV') !== 'production',
-                migrationsRun: configService.get('NODE_ENV') === 'production',
-            }),
-            inject: [ConfigService],
-        }),
-    ],
+    imports: [TypeOrmModule.forRoot(dataSourceOptions)],
 })
 export class DatabaseModule {}
